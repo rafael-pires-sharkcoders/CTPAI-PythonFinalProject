c1cf45fb825a77e5b88c766dc1ab6feddfe23eb0
rafael-pires-sharkcoders <149233319+rafael-pires-sharkcoders@users.noreply.github.com>
2025-06-25 16:32:17 +0100

    Add files via upload

---
+++ core/__init__.py
@@ -0,0 +1,8 @@
+"""
+Core module for the Object Detection System.
+
+This module contains the configuration management and core utilities.
+"""
+
+__version__ = "3.0.0"
+__author__ = "Enhanced Object Detection System" 
