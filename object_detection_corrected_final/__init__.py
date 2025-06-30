e99d64a99eae26e93ce48c70c30fb79a14cad3dd
rafael-pires-sharkcoders <149233319+rafael-pires-sharkcoders@users.noreply.github.com>
2025-06-30 17:40:42 +0100

    Delete object_detection_corrected_final directory

--- object_detection_corrected_final/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Core module for the Object Detection System.
-
-This module contains the configuration management and core utilities.
-"""
-
-__version__ = "3.0.0"
-__author__ = "Enhanced Object Detection System" 
